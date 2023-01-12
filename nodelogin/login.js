2c31fc542a84dc391f3abcbfbc94671d093ee01f
karmpatel960 <96860884+Karmpatel960@users.noreply.github.com>
2023-01-12 16:07:14 +0530

    package json updated

--- nodelogin/login.js
+++ nodelogin/login.js
@@ -39,53 +39,81 @@ app.get('/acc', function(request, response) {
 	 response.sendFile(path.join(__dirname + '/Create_Account.html'));
 });
 
-
-// http://localhost:3000/auth
 app.post('/auth', function(request, response) {
 	// Capture the input fields
 	let username = request.body.username;
 	let password = request.body.password;
 
-//    // Define the correct username and password
-//    const correctUsername = "1000"
-//    const correctPassword = "password"
+    // Define the correct username and password
+    const correctUsername = "1000"
+    const correctPassword = "password"
 
     // Check if the user's input matches the correct values
-
 	if (username && password) {
 		connection.query('SELECT * FROM loginuser.userdata WHERE username = ? AND Password = ?', [username, password], function(error, results, fields) {
 			// If there is an issue with the query, output the error
 			if (error) throw error;
 			// If the account exists
 			if (results.length > 0) {
-				// Authenticate the user
-				request.session.loggedin = true;
-				request.session.username = username;
-				// Redirect to home page
-				response.redirect('/home');
+				if (username === correctUsername && password === correctPassword) {
+				    request.session.loggedin = true;
+					if (request.session.loggedin) {
+						response.redirect('/admin', {}, function (err) {
+							if(err){
+								response.status(404).send("File not found");
+							}
+						});
+					} else {
+                     response.redirect('/');
+					}
+				} else {
+					// Authenticate the user
+					request.session.loggedin = true;
+					request.session.username = username;
+					// Redirect to home page
+					response.redirect('/home');
+				}
 			} else {
-				response.send('Incorrect Username and/or Password!');
+				 response.send('Invalid Username and Password!');
 			}
 			response.end();
 		});
-//	} else if (username === correctUsername && password === correctPassword) {
-//	                       if (request.session.loggedin) {
-//                                response.redirect('/admin'), {}, function (err) {
-//                                    if(err){
-//                                        response.status(404).send("File not found");
-//                                    }
-//                                });
-//                          }
-//  else {
-//                                response.status(401).send('Please login to view this page!');
-//                            }
-//
 
      } else {
 		response.send('Please enter Username and Password!');
 		response.end();
 	}
 });
+
+app.post('/register', (req, res) => {
+  const {Name,Username,email,password,passwordConfirm} = req.body;
+//  connection.query('SELECT email from loginuser.userdata WHERE email = ?', [email], async (err, results) => {
+//    if (err) {
+//      console.log(err);
+//    } else {
+//      if (results.length > 0) {
+//        return res.render("Create_Account", {
+//          message: 'The email is already in use'
+//        })
+//      } else if (password !== passwordConfirm) {
+//        return res.render("Create_Account", {
+//          message: 'Password dont match'
+//        });
+//      }
+//    }
+    const sysT = 'T';
+//    INSERT INTO `loginuser`.`userdata` (`Username`, `syst`, `Name`, `Password`, `Email`) VALUES ('900', 'T', 'Row', 'Roy4', 'row2');
+    connection.query('INSERT INTO loginuser.userdata SET ?', { Username:Username,sysT:sysT,Name: Name,Password: Password, Email: Email}, (err, results) => {
+      if (err) {
+        console.log(err);
+      } else {
+        return res.render("Create_Account", {
+          message: 'User registered'
+        });
+      }
+    });
+    res.redirect('/');
+  });
 
 
 app.get('/home', function(request,response){

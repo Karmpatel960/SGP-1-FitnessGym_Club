b3c4a0d69d02999986b55378b13aa46173ee4a26
karmpatel960 <96860884+Karmpatel960@users.noreply.github.com>
2023-02-27 23:23:04 +0530

    config added

---
+++ FGC_Web/config/config.js
@@ -0,0 +1,7 @@
+module.export = {
+   'facebookAuth':{
+   'clientID' :'8840207599385587',
+   'clientSecret':'9ab8272097058065098e272b3c976bfc',
+   'callbackURL':'https://localhost:3000/auth/facebook/callback'}
+
+}
